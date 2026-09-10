bec2c40fd4911093d28d29898d2181c73b05e20f
sachinp <sachinp@iitbhilai.ac.in>
2026-09-10 23:01:35 +0530

    hoisting -1

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-let a = 10;
-console.log(a); 
+// let a = 10;
+// console.log(a);
+
+// var a = 10;
+// var b = 20;
+// function add(x, y) {
+//   var sum = x + y;
+//   console.log(a);
+//   return sum;
+// }
+
+// var result = add(a, b);
+
+// console.log(result);
+
+// console.log(a);
+
+// let a = 10;
+// var b = 20;
+
+// var res = add(a, b);
+// console.log(res);
+
+// function add(x, y) {
+//   var sum = x + y;
+//   //   console.log(a);
+//   return sum;
+// }
