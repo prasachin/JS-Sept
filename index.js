ef2f6f5492d0fc988a751be7302f2c733b69dfbb
sachinp <sachinp@iitbhilai.ac.in>
2026-09-11 23:03:54 +0530

    data str + array

--- index.js
+++ index.js
@@ -26,3 +26,38 @@
 //   //   console.log(a);
 //   return sum;
 // }
+//
+// console.log(x);
+// var a ;
+// const b;
+// let c;
+// // Number, Array, Object, String, Boolean, Null, Undefined, Symbol, Set, Map, Date,
+// // let s = new Date();
+
+// console.log(c);
+
+// let a = 10;
+
+// let b = a;
+
+// b = 20;
+
+// console.log(a);
+// console.log(b);
+
+// let a = { x: 10 }; // (xyzhbg_a): {x:20}
+
+// let b = a; // (xyzhbg_a)
+
+// b.x = 20;
+
+// console.log(a.x);//(xyzhbg_a)
+// console.log(b.x);//(xyzhbg_a)
+
+// let a = [];
+
+// console.log(typeof a);
+
+// console.log(Array.isArray(a));
+
+// if(Array.isArray(a)) {}
