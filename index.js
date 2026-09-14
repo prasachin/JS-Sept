a95ee75ff4597cd3bdb80bf0af80428dc2d7285b
sachinp <sachinp@iitbhilai.ac.in>
2026-09-14 23:02:24 +0530

    array

--- index.js
+++ index.js
@@ -61,3 +61,130 @@
 // console.log(Array.isArray(a));
 
 // if(Array.isArray(a)) {}
+
+// function Array(x) {
+//   //   let v = [];
+//   //   v.push(1);
+//   //   v.push(2);
+//   //   return v;
+//   this.a = x;
+// }
+
+// let a = new Array(10);
+
+// // console.log(Array.isArray(a));
+// console.log(a.a);
+
+// let a = new Array(10);
+
+// console.log(a.length);
+
+// let a = [1, 2, 3, 4, 5];
+
+// console.log(a);
+// a.push(20); O(1)
+// console.log(a);
+
+// console.log(a);
+// a.unshift(20); O(n)
+// console.log(a);
+
+// console.log(a);
+// a.pop(); // O(1);
+// console.log(a);
+
+// console.log(a);
+// a.shift(); //O(n)
+// console.log(a);
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let b = a.slice(-2); // O(n)
+
+// // console.log(a);
+// console.log(b);
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let b = a.splice(3,2, 10, 20 , 30);
+
+// console.log(a);
+// console.log(b);
+
+// let a = [1, 22, 4, 50, 9, 10];
+
+// let c = ["1", "22", "4", "50", "9", "10"];
+// console.log(a);
+// let b = a.sort((x, y) => {
+//   return y - x;
+// });
+
+// console.log(a);
+
+// let a = [
+//   [1, 2, 3, 4],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log(a[0][0]);
+
+// let a = [1, 22, 4, 50, 9, 10];
+
+// for(let i = 0; i<a.length; i++) {
+//     console.log(a[i]);
+// }
+
+// let i = 0;
+
+// while(i<a.length) {
+//     console.log(a[i]);
+//     i++;
+// }
+
+// do {
+//   console.log(a[i]);
+//   i++;
+// } while (i < a.length);
+
+// let a = [1, 22, 4, 50, 9, 10];
+
+// function callMe(v, i) {
+//   //   console.log("I am called at index: ", i, " with value: ", v);
+//   //   return i;
+//   //   if (v === 50) {
+//   //     return v;
+//   //   }
+//   //   return true;
+// //   return v % 2 === 0;
+// }
+// let a = [1, 22, 4, 50, 9, 10];
+// function callMe(prev, curr, i) {
+//   console.log("I am called at index: ", i, " with value: ", v);
+//   return i;
+//   if (v === 50) {
+//     return v;
+//   }
+//   return true;
+//   return v % 2 === 0;
+//   return prev + curr;
+// }
+
+// let resForech = a.forEach(callMe);
+// console.log(a);
+// let reMap = a.map(callMe);
+
+// console.log(a);
+// console.log(reMap);
+
+// console.log(a);
+// console.log(resForech);
+
+// let resFilter = a.filter(callMe);
+
+// console.log(a);
+// console.log(resFilter);
+
+// let resReduce = a.reduce(callMe, 10);
+
+// console.log(resReduce);
